d6318fde7dcc09e7f95185c44a9b6ed5ed939151
Michał Wiśniewski <mikex929@gmail.com>
2021-07-18 20:16:49 +0200

    Add transform tables.

---
+++ chapter 6/3_Transform_tables.py
@@ -0,0 +1,52 @@
+def compute_transform_tables(X, Y, CC, CR, CD, CI):
+    """Time complexity: Θ(m*n), where m and n are lengths of words.
+
+    >>> X = 'ACAAGC'
+    >>> Y = 'CCGT'
+    >>> CC = -1
+    >>> CR = 1
+    >>> CD = CI = 2
+    >>> compute_transform_tables(X, Y, CC, CR, CD, CI)
+    ([[0, 2, 4, 6, 8], [2, 1, 3, 5, 7], [4, 1, 0, 2, 4], [6, 3, 2, 1, 3], [8, 5, 4, 3, 2], [10, 7, 6, 3, 4], [12, 9, 6, 5, 4]], [[None, 'I', 'I', 'I', 'I'], ['D', 'R', 'R', 'R', 'R'], ['D', 'C', 'C', 'I', 'I'], ['D', 'D', 'R', 'R', 'R'], ['D', 'D', 'R', 'R', 'R'], ['D', 'D', 'R', 'C', 'R'], ['D', 'C', 'C', 'D', 'R']])
+    """
+    X = ' ' + X
+    Y = ' ' + Y
+
+    m = len(X)
+    n = len(Y)
+
+    cost = []
+    operation = []
+
+    for _ in range(m):
+        cost.append([None] * n)
+        operation.append([None] * n)
+
+    cost[0][0] = 0
+
+    for i in range(1, m):
+        cost[i][0] = i * CD
+        operation[i][0] = 'D'
+    
+    for j in range(1, n):
+        cost[0][j] = j * CI
+        operation[0][j] = 'I'
+
+    for i in range(1, m):
+        for j in range(1, n):
+            if X[i] == Y[j]:
+                cost[i][j] = cost[i-1][j-1] + CC
+                operation[i][j] = 'C'
+            else:
+                cost[i][j] = cost[i-1][j-1] + CR
+                operation[i][j] = 'R'
+
+            if cost[i-1][j] + CD < cost[i][j]:
+                cost[i][j] = cost[i-1][j] + CD
+                operation[i][j] = 'D'
+
+            if cost[i][j-1] + CI < cost[i][j]:
+                cost[i][j] = cost[i][j-1] + CI
+                operation[i][j] = 'I'
+
+    return cost, operation
