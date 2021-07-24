228f9fcfd0ff36c53c4d90c8c0c99c0ad4f3fe1e
Michał Wiśniewski <mikex929@gmail.com>
2021-07-24 11:18:34 +0200

    Add Euclidean algorithm.

---
+++ chapter 7/1_Euclidean_algorithm.py
@@ -0,0 +1,21 @@
+from math import floor
+
+def greatest_common_divisor(a, b):
+    """Time complexity: O(lg b), where a and are integers.
+
+    >>> a = 10
+    >>> b = 0
+    >>> greatest_common_divisor(a, b)
+    (10, 1, 0)
+    >>> a = 30
+    >>> b = 18
+    >>> greatest_common_divisor(a, b)
+    (6, -1, 2)
+    """
+    if b == 0:
+        return a, 1, 0
+    
+    g, i_prim, j_prim = greatest_common_divisor(b, a % b)
+    i = j_prim
+    j = i_prim - floor(a / b) * j_prim
+    return g, i ,j
