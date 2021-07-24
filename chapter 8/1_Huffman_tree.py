9f40ef9980a542a0fc833ecc8c5ce1b17f6908c1
Michał Wiśniewski <mikex929@gmail.com>
2021-07-24 15:32:37 +0200

    Add heap to Huffman tree implementation.

--- chapter 8/1_Huffman_tree.py
+++ chapter 8/1_Huffman_tree.py
@@ -1,7 +1,10 @@
+from math import floor
+
+
 def build_huffman_tree(characters, frequencies, n):
     """Time complexity: O(n lg n), where n is number of uncompressed characters.
 
-    >>> characters = ['T', 'C', 'G', 'A']
+    >>> characters = ['A', 'C', 'G', 'T']
     >>> frequencies = [0.45, 0.05, 0.05, 0.45]
     >>> n = 4
     >>> build_huffman_tree(characters, frequencies, n)
@@ -14,11 +17,12 @@ def build_huffman_tree(characters, frequencies, n):
            'char': characters[i],
            'freq': frequencies[i]
         }
-        Q.append(z)
+        append_to_heap(Q, z)
 
     for i in range(n - 1):
-        x = get_min(Q)
-        y = get_min(Q)
+
+        x = pop_min_from_heap(Q)
+        y = pop_min_from_heap(Q)
 
         z = {
             'freq': x['freq'] + y['freq'],
@@ -26,17 +30,72 @@ def build_huffman_tree(characters, frequencies, n):
             '1': y
         }
 
-        Q.append(z)
+        append_to_heap(Q, z)
 
     return Q[0]
 
-def get_min(Q):
-    min_leaf_index = 0
+def append_to_heap(Q, z):
+    """Time complexity: O(lgn), n - number of heap's elements.
 
-    for i in range(1, len(Q)):
-        if Q[i]['freq'] < Q[min_leaf_index]['freq']:
-            min_leaf_index = i
-    
-    min_leaf = Q.pop(min_leaf_index)
+    >>> Q = [{'char': 'C', 'freq': 0.05}, {'char': 'A', 'freq': 0.45}, {'char': 'T', 'freq': 0.45}]
+    >>> z = {'char': 'G', 'freq': 0.05}
+    >>> append_to_heap(Q, z)
+    >>> Q
+    [{'char': 'C', 'freq': 0.05}, {'char': 'G', 'freq': 0.05}, {'char': 'T', 'freq': 0.45}, {'char': 'A', 'freq': 0.45}]
+    """
+    Q.append(z)
+    element_index = len(Q) - 1
+    parent_index = floor((element_index - 1) / 2)
 
-    return min_leaf
+    while element_index != 0 and Q[parent_index]['freq'] > Q[element_index]['freq']:
+        temp = Q[parent_index]
+        Q[parent_index] = Q[element_index]
+        Q[element_index] = temp
+
+        element_index = parent_index
+        parent_index = floor((element_index - 1) / 2)
+
+def pop_min_from_heap(Q):
+    """Time complexity: O(lgn), n - number of heap's elements.
+
+    >>> Q = [{'char': 'C', 'freq': 0.05}, {'char': 'G', 'freq': 0.05}, {'char': 'T', 'freq': 0.45}, {'char': 'A', 'freq': 0.45}]
+    >>> pop_min_from_heap(Q)
+    {'char': 'C', 'freq': 0.05}
+    >>> Q
+    [{'char': 'G', 'freq': 0.05}, {'char': 'A', 'freq': 0.45}, {'char': 'T', 'freq': 0.45}]
+    """
+    if len(Q) == 1:
+        return Q.pop(0)
+
+    u = Q[0]
+    last_index = len(Q) - 1
+    Q[0] = Q.pop(last_index)
+    last_index -= 1
+
+    element_index = 0
+
+    while True:
+        left_child_index = 2 * element_index + 1
+        right_child_index = 2 * element_index + 2
+
+        if (
+            right_child_index <= last_index and 
+            Q[right_child_index]['freq'] < Q[element_index]['freq'] and
+            Q[right_child_index]['freq'] < Q[left_child_index]['freq']
+        ):
+            temp = Q[element_index]
+            Q[element_index] = Q[right_child_index]
+            Q[right_child_index] = temp
+            element_index = right_child_index
+        elif (
+            left_child_index <= last_index and
+            Q[left_child_index]['freq'] < Q[element_index]['freq']
+        ):
+            temp = Q[element_index]
+            Q[element_index] = Q[left_child_index]
+            Q[left_child_index] = temp
+            element_index = left_child_index
+        else:
+            break
+
+    return u
